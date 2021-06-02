47b674b14510956973cafbb8a7eabc39db658266
gal kubani <galkubani@gmail.com>
2021-06-02 11:24:22 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const express=require('express')
-const cors= require('cors')
-const port= process.env.PORT
+const express = require('express')
+const cors = require('cors')
+const port = process.env.PORT || 3000
 
-const app= express()
+const app = express()
 require('./db/mongoose')
-const scrapeRouter=require('./routers/scrapePageRouter')
+const scrapeRouter = require('./routers/scrapePageRouter')
 app.use(cors())
 app.use(express.json())
 app.use(scrapeRouter)
-app.use('/',(req,res)=>{
+app.use('/', (req, res) => {
     res.send("ok")
 })
 
-app.listen(port,()=> console.log("server on port:", port))
+app.listen(port, () => console.log("server on port:", port))
